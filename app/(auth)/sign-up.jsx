119bf0f680378b38b8bda4da37a5422fa86d1fdf
Asanka-GAP <asankapradeep0830@gmail.com>
2024-07-15 01:26:08 +0530

    app-write-logic-implementation

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,24 +1,38 @@
-import { ScrollView, Image, View, Text } from "react-native";
+import { ScrollView, Image, View, Text, Alert } from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
 import FormField from "../../components/FormField";
 import CustomButton from "../../components/CustomButton";
-import { Link } from "expo-router";
+import { Link, router } from "expo-router";
+import { createUser } from "../../lib/appwrite";
 
 const SignUp = () => {
-
   const [form, setForm] = useState({
-    username:'',
-    email:'',
-    password:''
-  })
+    username: "",
+    email: "",
+    password: "",
+  });
 
-const [isSubmitting, setisSubmitting] = useState(false)
+  const [isSubmitting, setisSubmitting] = useState(false);
 
-  const submit = ()=>{
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert('Error','Please fill in all the fields')
+    }
 
-  }
+    setisSubmitting(true);
+
+    try {
+      const result = await createUser(form.email,form.password,form.username);
+
+    router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error',error.message)
+    }finally{
+      setisSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -36,48 +50,44 @@ const [isSubmitting, setisSubmitting] = useState(false)
             Sign up to Aora
           </Text>
           <FormField
-          title="Username"
-          value={form.username}
-          handleChangeText={(e)=>setForm({...form,
-            username:e
-          })}
-          otherStyles="mt-7"
-          keyBoardType="email-address"
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-7"
+            keyBoardType="email-address"
           />
           <FormField
-          title="Email"
-          value={form.email}
-          handleChangeText={(e)=>setForm({...form,
-            email:e
-          })}
-          otherStyles="mt-7"
-          keyBoardType="email-address"
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyBoardType="email-address"
           />
           <FormField
-          title="Password"
-          value={form.password}
-          handleChangeText={(e)=>setForm({...form,
-            password:e
-          })}
-          otherStyles="mt-7"
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
           />
           <CustomButton
-          title="Sign Up"
-          handlePress={submit}
-          containerStyles="mt-7"
-          isLoading={isSubmitting}
+            title="Sign Up"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
           />
 
           <View className="justify-center pt-5 flex-row gap-2">
-<Text className="text-lg text-gray-100 font-pregular">
-  Have an account already?
-</Text>
-<Link href="/sign-in" className="text-lg
-font-psemibold text-secondary">
-Sign In
-</Link>
+            <Text className="text-lg text-gray-100 font-pregular">
+              Have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg
+font-psemibold text-secondary"
+            >
+              Sign In
+            </Link>
           </View>
-
         </View>
       </ScrollView>
     </SafeAreaView>
