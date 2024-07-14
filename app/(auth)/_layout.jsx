561aeb4c72d9bbccd58b4d5c8705cc64cb36881d
Asanka-GAP <asankapradeep0830@gmail.com>
2024-07-14 20:07:30 +0530

    first commit

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AuthLayot = () => {
+  return (
+    <View>
+      <Text>AuthLayot</Text>
+    </View>
+  )
+}
+
+export default AuthLayot
+
+const styles = StyleSheet.create({})
